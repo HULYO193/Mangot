184046613262c17760e6a68058e1b280f8be0702
HULYO <hulibr12@gmail.com>
2023-05-22 14:13:51 +0300

    PlanForCode - Line 62

--- app/src/main/java/com/example/mangot/Reader.java
+++ app/src/main/java/com/example/mangot/Reader.java
@@ -81,7 +81,7 @@ public class Reader extends AppCompatActivity {
 
                         recyclerreader.setAdapter(adapter);
 
-
+///
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
